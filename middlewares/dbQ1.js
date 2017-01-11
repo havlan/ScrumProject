efb189f245694bcee9a1fcd598984617ecba4d8d
torsku <torsku@student.hist.no>
2017-01-11 12:47:24 +0100

    getEmployeesWorking()

--- middlewares/dbQ1.js
+++ middlewares/dbQ1.js
@@ -6,6 +6,9 @@ module.exports = {
         getUserInfo : function (req, res){
             dbHelper.dbQuery(req, res, "select * from NodeETest");
             //console.log(JSON.stringify(res));
+        },
+        getEmployeesWorking : function(req,res,datefrom,dateto){
+            dbHelper.dbQuery(req,res,"SELECT e.Name, s.date, e.employee_id FROM Employee e, Shift s, shift_has_employee she WHERE e.employee_id = she.employee_id AND s.shift_id = she.shift_id AND s.date between"+datefrom+  "and"+dateto);
         }
 }
 
