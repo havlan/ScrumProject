0161fac83de7d2aea08eef40b8418cc1f6736d64
zenaxin <o_a_leknes@hotmail.com>
2017-01-17 09:41:18 +0100

    Merge branch 'restAPI' of C:\Users\o_A_l\WebstormProjects\scrumLords2017 with conflicts.

--- middlewares/dbQ1.js
+++ middlewares/dbQ1.js
@@ -48,7 +48,11 @@ module.exports = {
     getEmployee_shifts_toCurrentDate : function(req,res){
         dbHelper.dbQuery(req,res,"select * from Employee_shifts_toCurrentDate where employee_id =?",req.params.employee_id);
     },
-
+    getVaktoversikt : function(req,res){
+        var get = {name:req.body.name, type:req.body.type, department:req.body.department, responsibility_allowed:req.body.type, phone_nr:req.body.phone_nr};
+        console.log("Overview worklist");
+        dbHelper.getdbQuery(req,res, "overview shifts ?", get);
+    },
 
     //POST/PUT
 
@@ -102,49 +106,33 @@ module.exports = {
     },
 
     //update
-    //SYNTAKS
-    //var pk = req.body.primarykey;
-    //dbHelper.postdbQuery(req,res,"update TABLE set ? where PRIMARY KEY = ?",[{variabel : req.body.variabelnavn,.... },pk]);
-
     updateShift_has_employee : function (req, res) {
-        var pk = req.body.shift_id;
-        var pk2 = req.body.employee_id;
-        dbHelper.postdbQuery(req,res,"update shift_has_employee set ? where employee_id = ? and shift_id = ?",[{avalibility:req.body.avalibility},pk,pk2]);
+        dbHelper.postdbQuery(req,res,"update shift_has_employee set avalibility = ?,employee_id = ?,shift_id = ? where employee_id = ? and shift_id = ?",[req.body.avalibility,req.body.employee_id,req.body.shift_id,req.body.employee_id,req.body.shift_id]);
     },
     updateEmployee : function (req, res) {
-        var id = req.body.employee_id;
-        dbHelper.postdbQuery(req,res,"update Employee set ? where employee_id = ?",[{phone_nr : req.body.phone_nr, total_hours:req.body.total_hours,
-            email:req.body.email, seniority : req.body.seniority, responsibility_allowed: req.body.responsibility_allowed, address: req.body.address, pers_id: req.body.pers_id}, id]);
+        dbHelper.postdbQuery(req,res, "update Employee set phone_nr = ?,email= ?, address = ? where employee_id = ? ",[req.body.phone_nr,req.body.email,req.body.address,req.body.employee_id]);
+        //dbHelper.postdbQuery(req,res,"update Employee set phone_nr=?,total_hours=?,employee_id=?,email=?,seniority=?,responsibility_allowed=?,type_name=?,name=?,address=?,pers_id=? where employee_id = ?",[req.body.phone_nr,req.body.total_hours,req.body.employee_id,req.body.email,req.body.seniority,req.body.responsibility_allowed,req.body.type_name,req.body.name,req.body.address,req.body.pers_id,req.body.employee_id]);
     },
     updateType : function(req,res){
-        var pk = req.body.name;
-        dbHelper.postdbQuery(req,res,"update Type set ? where name = ?",[{rank:req.body.rank},pk]);
+        dbHelper.postdbQuery(req,res,"update Type set rank = ? where name = ?",[req.body.rank,req.body.name]);
     },
     updateShift : function (req, res) {
-        var pk = req.body.shift_id;
-        dbHelper.postdbQuery(req,res,"update Shift set ? where shift_id = ?",[{minutes:req.body.minutes,date:req.body.date,department_id:req.body.department_id,type_name:req.body.type_name},pk]);
+        dbHelper.postdbQuery(req,res,"update Shift set minutes=?,date=?,department_id=?,type_name=? where shift_id=?",[req.body.minutes,req.body.date,req.body.department_id,req.body.type_name,req.body.shift_id]);
     },
     updateDepartment : function (req, res) {
-        var pk = req.body.department_id;
-        dbHelper.postdbQuery(req,res,"update Department set ? where department_id=?",[{department_name:req.body.department_name},pk]);
+        dbHelper.postdbQuery(req,res,"update Department set department_name =? where department_id=?",[req.body.department_name,req.body.department_id]);
     },
     updateRequest : function (req, res) {
-        var pk = request_id;
-        dbHelper.postdbQuery(req,res,"update Request set ? where request_id =?",[{shift_id:req.body.shift_id,employee_id:req.body.employee_id,checked_by_admin:req.body.checked_by_admin},pk]);
+        dbHelper.postdbQuery(req,res,"update Request set shift_id = ?, employee_id =?, checked_by_admin =? where request_id =?",[req.body.shift_id,req.body.employee_id,req.body.checked_by_admin,req.body.request_id]);
     },
     updateAbsence : function (req, res) {
-        var pk = req.body.absence_id;
-        dbHelper.postdbQuery(req,res,"update Absence set ? where absence_id =?",[{checked_by_admin:req.body.checked_by_admin,shift_id:req.body.shift_id,employee_id:req.body.employee_id},pk]);
+        dbHelper.postdbQuery(req,res,"update Absence set checked_by_admin =?,shift_id=?,employee_id=? where absence_id =?",[req.body.checked_by_admin,req.body.shift_id,req.body.employee_id,req.body.absence_id]);
     },
     updateOvertime : function (req, res) {
-        var pk = req.body.overtime_id;
-        dbHelper.postdbQuery(req,res,"update Overtime set ? where overtime_id=?",[{checked_by_admin:req.body.checked_by_admin,shift_id:req.body.shift_id,employee_id:req.body.employee_id,overtime:req.body.overtime},pk]);
+        dbHelper.postdbQuery(req,res,"update Overtime set checked_by_admin=?,shift_id=?,employee_id=?,overtime=? where overtime_id=?",[req.body.checked_by_admin,req.body.shift_id,req.body.employee_id,req.body.overtime,req.body.overtime_id]);
     },
     updateLogInInfo : function (req, res) {
-        var pk = req.body.Username;
-        dbHelper.postdbQuery(req,res,"update LoginInfo set ? where Username=?",[{is_admin:req.body.is_admin},pk]);
+        dbHelper.postdbQuery(req,res,"update LoginInfo set is_admin=? where Username=?",[req.body.is_admin,req.body.Username]);
     }
-
-
 }
 //heihei
