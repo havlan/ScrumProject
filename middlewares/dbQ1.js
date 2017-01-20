1cb768599af957a79ac3365e71e6974bbfb01bea
zenaxin <o_a_leknes@hotmail.com>
2017-01-20 14:53:17 +0100

    vaktliste, vaktoversikt, vaktbytte

--- middlewares/dbQ1.js
+++ middlewares/dbQ1.js
@@ -57,7 +57,7 @@ module.exports = {
         dbHelper.simpleLogin("select * from LoginInfo where Username = ?", [username]);
     },*/
     getVaktliste : function(req,res){
-        dbHelper.getdbQuery(req, res, "select * from EMPLOYEE_GIVEN_SHIFT where ShiftStart like '2017-03-31%' and department_id = 5;"); //,[req.params.ShiftStart, req.params.department_id]
+        dbHelper.getdbQuery(req, res, "select * from WORKSHIFTTOGETHER where ShiftStart like ? and department_id = ?",[req.params.ShiftStart, req.params.department_id]);
     },
     getTypeNames : function (req, res) {
         dbHelper.getdbQuery(req,res,"Select name from Type;")
@@ -255,6 +255,16 @@ module.exports = {
     updateLogInInfo: function (req, res) {
         var pk = req.body.username;
         dbHelper.postdbQuery(req, res, "update LoginInfo set ? where Username=?", [{is_admin: req.body.is_admin}, pk]);
-    }
+    },
+    getVaktbytte: function(req, res){
+        dbHelper.getdbQuery(req, res, "select * from Shift where shift_id=?");
+    },
+    postnewVaktbytte: function (req, res){
+        var post = {
+
+        }
+    },
+
+
 }
 //heihei
