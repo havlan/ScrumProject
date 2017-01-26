4e21d449caee8fc6b940a00a8621c3fcc1007579
torsku <torsku@student.hist.no>
2017-01-26 10:18:26 +0100

    fishy fish that smells of something fishy

--- public/frontendJS/overviewEmp.js
+++ public/frontendJS/overviewEmp.js
@@ -2,7 +2,7 @@
  * Created by rebekkaheggebo on 20.01.2017.
  */
 var myList = [];
-
+//hei
 $.get('/getEmployeeRestricted', {}, function(req, res, data){
     console.log(data);
     console.log(data.responseJSON[0]);
