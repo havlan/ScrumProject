edcc0e3e9aefc7b37f630145445ec37fae753886
rebekkaheggebo <rheggebo@hotmail.com>
2017-01-29 19:34:00 +0100

    comments

--- public/frontendJS/frontpageAdmin.js
+++ public/frontendJS/frontpageAdmin.js
@@ -1,20 +1,41 @@
-
+/**
+ * Dislays the number of shifts that need to be filled
+ * @function
+ */
 $.get('/getAvailableShifts', {}, function(req, res, data){
     document.getElementById("freeShiftsNumber").innerHTML = data.responseJSON[0].total;
 });
+
+/**
+ * Displays the number of leave/absence to be approved
+ * @function
+ */
 $.get('/getAbsenceNum', {}, function(req, res, data){
     document.getElementById("absenceWarning").innerHTML = data.responseJSON[0].total + " fravær til godkjenning";
 });
+
+/**
+ * Displays the number of overtime request to be approved
+ * @function
+ */
 $.get('/getOvertimeNum', {}, function(req, res, data){
     document.getElementById("overtimeWarning").innerHTML = data.responseJSON[0].total + " overtid til godkjenning";
 });
+
+/**
+ *Displays the number of shift changes to be approved
+ * @function
+ */
 $.get('/getChangeNum', {}, function(req, res, data){
     document.getElementById("changeWarning").innerHTML = data.responseJSON[0].total + " bytteforespørsel";
 });
 
 
 var event_id;
-
+/**
+ * Displays the calendar (Fullcalendar)
+ * @function
+ */
 $(document).ready(function() {
 
     $('#calendar').fullCalendar({
@@ -55,19 +76,24 @@ $(document).ready(function() {
         ,
         eventClick:  function(event, jsEvent, view) {
             event_id = event.id;
+            //if the shift is green it is approved by the employee, adds correct modal
             if(event.phone_nr) {
                 $('#modalShift').html(event.title);
                 $('#department').html("Avdeling: " + event.description);
                 $('#begin').html("Starter: " + moment(event.start).format('MMM Do h:mm A'));
                 $('#end').html("Slutter: " + moment(event.end || event.start).format('MMM Do h:mm A'));
                 $('#fullCalModal').modal();
-            } else if(event.email){
+            }
+            //if the shift is blue it has already passed, adds correct modal
+            else if(event.email){
                 $('#modalShiftDone').html(event.title);
                 $('#departmentDone').html("Avdeling: " + event.description);
                 $('#beginDone').html("Startet: " + moment(event.start).format('MMM Do h:mm A'));
                 $('#endDone').html("Sluttet: " + moment(event.end || event.start).format('MMM Do h:mm A'));
                 $('#fullCalModalDone').modal();
-            } else{
+            }
+            //if the shift is yellow it is an available shift for the employee, adds correct modal
+            else{
                 $('#modalShiftFree').html(event.title);
                 $('#departmentFree').html("Avdeling: " + event.description);
                 $('#beginFree').html("Starter: " + moment(event.start).format('MMM Do h:mm A'));
@@ -80,11 +106,19 @@ $(document).ready(function() {
 
 });
 
+/**
+ * Gets an employee's shifts from a current date
+ * @functin
+ */
 $.get('/getEmployee_Shifts_fromCurrentDate2',{},function (req,res,data) {
     myList = data.responseJSON;
     buildHtmlTable('#vaktTable');
 });
-
+/**
+ * Builds the table with data from the database
+ * @param selector
+ * @param list
+ */
 function buildHtmlTable(selector,list) {
     list = myList;
     var columns = addAllColumnHeaders(list, selector);
@@ -104,6 +138,12 @@ function buildHtmlTable(selector,list) {
     }
     $(selector).append(tbody);
 }
+/**
+ * Adds column headers to the table
+ * @param myList
+ * @param selector
+ * @returns {Array}
+ */
 function addAllColumnHeaders(myList, selector) {
     var columnSet = [];
     var headerThead$ = $('<thead/>');
@@ -123,7 +163,10 @@ function addAllColumnHeaders(myList, selector) {
     $(headerThead$).append(headerTr$);
     return columnSet;
 }
-//When pressing 'Lagre'-button any row that is checked will get checked_by_admin=1
+/**
+ * When pressing 'Lagre'-button any row that is checked will get checked_by_admin=1
+ * @function
+ */
 $(document).on('click','#sendForespørsel',function (e) {
     e.preventDefault();
     var data = $("#vaktTable").find("input:checkbox:checked").map(function () {
@@ -135,7 +178,7 @@ $(document).on('click','#sendForespørsel',function (e) {
             type: 'POST',
             data: {'shift_id': data[i], 'checked_by_admin': 0,'explanation_request': ""},
             success: function (data) {
-                document.getElementById("successMessage").innerHTML = "Success!";
+                document.getElementById("successMessage").innerHTML = "Suksess";
                 showSuccessMessage();
             },
             error: function(xhr){
@@ -151,6 +194,10 @@ $(document).on('click','#sendForespørsel',function (e) {
     }
 });
 
+/**
+ * Sends request to change shift
+ * @function
+ */
 $(document).on('click','#freeSave',function (e) {
     var textinput = $("#expField").val();
     if(textinput.length<300){
@@ -159,7 +206,7 @@ $(document).on('click','#freeSave',function (e) {
             type: 'POST',
             data: {'shift_id': event_id,'explanation_request': textinput, 'checked_by_admin': 0},
             success: function (data) {
-                document.getElementById("successMessage").innerHTML = "Success!";
+                document.getElementById("successMessage").innerHTML = "Forespørsel om å bytte vakt sendt";
                 showSuccessMessage();
             },
             error: function(xhr){
@@ -177,7 +224,10 @@ $(document).on('click','#freeSave',function (e) {
     }
 });
 
-
+/**
+ * Sends request to take available shift
+ * @function
+ */
 $(document).on('click','#sendRequest',function () {
     if (event_id!=null){
         $.ajax({
@@ -185,7 +235,7 @@ $(document).on('click','#sendRequest',function () {
             type: 'POST',
             data: {'shift_id': event_id},
             success: function (data) {
-                document.getElementById("successMessage").innerHTML = "Success!";
+                document.getElementById("successMessage").innerHTML = "Forespørsel om å ta vakt sendt";
                 showSuccessMessage();
             },
             error: function(xhr){
@@ -204,6 +254,10 @@ $(document).on('click','#sendRequest',function () {
     }
 });
 
+/**
+ * Sends overtime request
+ * @function
+ */
 $(document).on('click','#overSave',function () {
 
     var numberinput = $("#overTime").val();
@@ -218,7 +272,8 @@ $(document).on('click','#overSave',function () {
                     'overtime': numberinput,
                     'explanation': explenation},
                 success: function (data) {
-                    alert("success!");
+                    document.getElementById("successMessage").innerHTML = "Forespørsel om overtid sendt";
+                    showSuccessMessage();
                 },
                 error: function(xhr){
                     if(xhr.status==404){
@@ -234,16 +289,27 @@ $(document).on('click','#overSave',function () {
     }
 });
 
-
+/**
+ *  Displays the "free-field" in the "green" modal
+ *  @function
+ */
 function showExplanationField(){
     document.getElementById('spaceFree').style.visibility = "visible";
     document.getElementById('spaceOver').style.visibility="hidden";
 }
+/**
+ * Displays the "overtime-field" in the "green" modal
+ * @function
+ */
 function showOverField(){
     document.getElementById('spaceFree').style.visibility = "hidden";
     document.getElementById('spaceOver').style.visibility="visible";
 }
 
+/**
+ * Displays a success message
+ * @function
+ */
 function showSuccessMessage() {
     var element = document.getElementById('successMessageBox');
     element.style.display = "block";
@@ -251,6 +317,10 @@ function showSuccessMessage() {
         element.style.display = "none";
     }, 3000);
 }
+/**
+ * Displays an error message
+ * @function
+ */
 function showErrorMessage() {
     var element = document.getElementById('errorMessageBox');
     element.style.display = "block";
@@ -258,7 +328,10 @@ function showErrorMessage() {
         element.style.display = "none";
     }, 3000);
 }
-
+/**
+ * Displays a warning message
+ * @function
+ */
 function showWarningMessage() {
     var element = document.getElementById('warningMessageBox');
     element.style.display = "block";
