2adc89737dff3fa8fcb9c88dd5ec93b483ce67cc
tjabejohannes <tjabe_johannes@outlook.com>
2017-01-29 17:10:15 +0100

    Merge remote-tracking branch 'remotes/origin/master' into frontEnd
    # Conflicts:
    #	controllers/configRoute.js
    #	package.json
    #	public/css/login.css
    #	public/frontendJS/shiftOverview.js
    #	views/login.html

--- documentation.js
+++ documentation.js
@@ -11,9 +11,9 @@ var documentation = require('documentation');
 var fs = require('fs');
 
 
-documentation.build(['/Users/LittleGpNator/WebstormProjects/scrumLords2017/public/frontendJS/approvalAdmin.js'], {}, function (err, res) {
+documentation.build(['./public/frontendJS/approvalAdmin.js'], {}, function (err, res) {
     documentation.formats.md(res, {}, function(err, output) {
-        fs.writeFileSync('./approvalAdmin.md', output);
+        fs.writeFileSync('./documentation/approvalAdmin.md', output);
     });
 });
 
